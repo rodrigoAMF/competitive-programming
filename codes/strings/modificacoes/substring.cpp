5400a135955f7f6da192170bcfd4b7c3f62ec60a
rodrigoAMF <rodrigoamf@outlook.com>
2019-01-30 11:09:11 -0200

    Update: Adicionado outra forma de usar substr
    Adicionado também como a função deve ser usada

--- codes/strings/modificacoes/substring.cpp
+++ codes/strings/modificacoes/substring.cpp
@@ -1,1 +1,3 @@
+substr(pos, len) // (Posicão inicial, Quantidade de caracteres para pegar)
 string str1 = line.substr(0,meio);
+string str1 = line.substr(1); // Pega tudo do primeiro caracter em diante
